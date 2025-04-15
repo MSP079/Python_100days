6c66b606978b489a5fff0d0b221d49560ebc848b
MSP079 <133376336+MSP079@users.noreply.github.com>
2025-04-15 12:08:55 +0530

    Project1

---
+++ Project1.brandNameGenerator/task.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city = input("Which city did your grow up in ?")
+pet = input("What is the name of the pet?")
+print("Your band name could be: " + city + " " + pet)
