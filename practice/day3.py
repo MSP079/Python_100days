a037bb6dafda07a0f0e2abda01b9061efa120c29
MSP079 <133376336+MSP079@users.noreply.github.com>
2025-04-27 23:24:03 +0530

    day4

--- practice/day3.py
+++ practice/day3.py
@@ -61,6 +61,8 @@ if height >=120:
     elif age <= 18:
         bill = 7
         print("Youth tickets are $7.")
+    elif age >=45 and age <=55:
+        print("Everthing is going to be ok. Have a free rider on us!")
     else:
         bill = 12
         print("Adults tickets are $12.")
@@ -71,3 +73,6 @@ if height >=120:
     print(f"Your final bill is ${bill}")
 else:
     print("Sorry you have to grow taller before you can ride")
+
+
+#multiple conditions in if statement using logical operators (and, or, not)
